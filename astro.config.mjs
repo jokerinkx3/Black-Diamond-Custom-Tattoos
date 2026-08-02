80f72ceb6603333562a0f4b2c0d0bccc50bec33d
jokerinkx3 <jokerinkx3@gmail.com>
2026-08-01 18:23:21 -0700

    Configure Astro for the actual Wix Headless deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
+// @ts-check
 import { defineConfig, envField } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import wix from "@wix/astro";
 import wixPages from "@wix/astro-pages";
+import cloudProviderFetchAdapter from "@wix/cloud-provider-fetch-adapter";
+
+const isBuild = process.env.NODE_ENV === "production";
 
 export default defineConfig({
-  site: "https://bdcustomtattoos.wixstudio.com/bdct-headless-stagin",
+  site: "https://h6s-92e874502bd0c4-bdcustomtattoos.wix-site-host.com",
   output: "server",
   integrations: [wix(), wixPages(), react(), sitemap()],
   security: {
@@ -21,5 +25,6 @@ export default defineConfig({
         access: "public"
       })
     }
-  }
+  },
+  ...(isBuild && { adapter: cloudProviderFetchAdapter({}) })
 });
